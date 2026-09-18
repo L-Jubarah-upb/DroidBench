f2ca56870e527f91cc8371b1d99a08159b73fd34
L-Jubarah-upb <leen.jubarah@upb.de>
2026-09-18 11:01:07 +0200

    removed irrelevant source

--- projects/InterAppCommunication/StartActivityForResult1/app/src/main/java/org/cert/WriteFile/MainActivity.java
+++ projects/InterAppCommunication/StartActivityForResult1/app/src/main/java/org/cert/WriteFile/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends Activity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {			
 	  if (resultCode == 0 && requestCode == 0 && data != null) {
 	    if (data.hasExtra("secret")) {
-	    	if(data.getExtras().getString("secret") != null){ //source
+	    	if(data.getExtras().getString("secret") != null){
 		    	String filename = "sinkFile.txt";
 		    	String sinkData = data.getExtras().getString("secret");		// another source
 		    	FileOutputStream outputStream;	
